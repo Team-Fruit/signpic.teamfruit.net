6d88abd1c02935ee90e2176f2b9daceefeefc10b
sjcl <fruit@bebehp.com>
2018-12-02 19:42:45 +0900

    right navbar toggle button

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,8 @@
 <template>
 <b-navbar toggleable="md" type="light" variant="light" fixed="top">
     <b-container>
-        <b-navbar-toggle target="nav_text_collapse"></b-navbar-toggle>
         <b-navbar-brand to="/">SignPicture</b-navbar-brand>
+        <b-navbar-toggle target="nav_text_collapse"></b-navbar-toggle>
         <b-collapse is-nav id="nav_text_collapse">
             <b-navbar-nav>
                 <b-nav-item to="/analytics">Analytics</b-nav-item>
